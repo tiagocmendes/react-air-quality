a357e4f451f795024bded9adfc6a85f88d544aef
tiagocmendes <tiagocmendes@ua.pt>
2020-04-12 05:53:02 +0100

    Finished country details

--- src/views/Continents/CountryDetails.js
+++ src/views/Continents/CountryDetails.js
@@ -7,7 +7,9 @@ import IconButton from '@material-ui/core/IconButton';
 
 import Card from "components/Card/Card.js";
 import CardAvatar from "components/Card/CardAvatar.js";
+import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
+import Table from "components/Table/Table.js";
 
 import Countries from "views/Continents/Countries.js";
 
@@ -19,6 +21,7 @@ class CountryDetails extends React.Component {
         super(props);
         this.state = {
             airQuality: null,
+            pollutants: [],
             return: false
         }
         this.continent = props.continent;
@@ -39,7 +42,28 @@ class CountryDetails extends React.Component {
             fontSize: "20px",
             fontWeight: "500",
             marginLeft: "33%"
+        },
+        cardCategoryWhite: {
+            color: "rgba(255,255,255,.62)",
+            margin: "0",
+            fontSize: "14px",
+            fontWeight: "500",
+            marginTop: "0",
+            marginBottom: "0"
+        },
+        cardTitleWhite: {
+            color: "#FFFFFF",
+            marginTop: "0px",
+            minHeight: "auto",
+            fontWeight: "500",
+            fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+            marginBottom: "3px",
+            textDecoration: "none"
+        },
+        cardHeader: {
+            backgroundColor: "#4caf50"
         }
+
     }
 
     componentDidMount() {
@@ -56,9 +80,22 @@ class CountryDetails extends React.Component {
 
             })
             .then(data => {
-                console.log(data);
+
+                var pollutants = [];
+                pollutants.push(data[0].pollutants);
+
+                pollutants = Object.entries(pollutants[0]);
+                
+                var table = [];
+                pollutants.forEach(element => table.push([
+                    <i className="fas fa-atom"></i>,
+                    element[0],
+                    element[1].v
+                ]))
+
                 this.setState({
                     airQuality: data[0],
+                    pollutants: table,
                     return: this.state.return
                 })
 
@@ -87,26 +124,43 @@ class CountryDetails extends React.Component {
                             <strong>Air Quality Details</strong></h3>
                     </GridItem>
                     {this.state.airQuality !== null ?
-                    <GridItem xs={12} sm={12} md={4} style={{ marginTop: "10px" }}>
-                        <Card profile>
-                            <CardAvatar profile>
-                                <a href="/">
-                                    <img style={{ borderRadius: "50%", height: "100px", width: "100px" }} src={this.country.flag} alt="..." />
-                                </a>
-                            </CardAvatar>
-                            <CardBody profile>
-                                <div style={this.classes.aqi}>{this.state.airQuality.aqi}</div>
-                                <h3>{this.country.name}</h3>
-                                <span><strong>Air Quality Indicator:</strong> {this.state.airQuality.aqi}</span><br />
-                                <span><strong>Station:</strong> {this.state.airQuality.name}</span><br />
-                                <span><strong>Latitude:</strong> {this.state.airQuality.latitude}</span><br />
-                                <span><strong>Latitude:</strong> {this.state.airQuality.longitude}</span><br />
-                                <span><strong>Primary pollutant:</strong> {this.state.airQuality.primaryPollutant}</span><br />
-                                <span><strong>Last update:</strong> {this.state.airQuality.time + " @ " + this.state.airQuality.timeZone}</span><br />
+                        <GridItem xs={12} sm={12} md={4} style={{ marginTop: "10px" }}>
+                            <Card profile>
+                                <CardAvatar profile>
+                                    <a href="/">
+                                        <img style={{ borderRadius: "50%", height: "100px", width: "100px" }} src={this.country.flag} alt="..." />
+                                    </a>
+                                </CardAvatar>
+                                <CardBody profile>
+                                    <div style={this.classes.aqi}>{this.state.airQuality.aqi}</div>
+                                    <h3>{this.country.name}</h3>
+                                    <span><strong>Air Quality Indicator:</strong> {this.state.airQuality.aqi}</span><br />
+                                    <span><strong>Station:</strong> {this.state.airQuality.name}</span><br />
+                                    <span><strong>Latitude:</strong> {this.state.airQuality.latitude}</span><br />
+                                    <span><strong>Latitude:</strong> {this.state.airQuality.longitude}</span><br />
+                                    <span><strong>Primary pollutant:</strong> {this.state.airQuality.primaryPollutant}</span><br />
+                                    <span><strong>Last update:</strong> {this.state.airQuality.time + " @ " + this.state.airQuality.timeZone}</span><br />
+                                </CardBody>
+                            </Card>
+                        </GridItem>
+                        : ""}
+
+                    <GridItem xs={12} sm={12} md={8}>
+                        <Card>
+                            <CardHeader style={this.classes.cardHeader}>
+                                <h4 style={this.classes.cardTitleWhite}><i className="fas fa-smog"></i>  Other Pollutants</h4>
+                                <p style={this.classes.cardCategoryWhite}>Concentration of different pollutions in the air</p>
+                            </CardHeader>
+                            <CardBody>
+                                <Table
+                                    tableHeaderColor="success"
+                                    tableHead={["", "Pollutant", "Value"]}
+                                    tableData={this.state.pollutants}
+                                />
                             </CardBody>
                         </Card>
+
                     </GridItem>
-                    : "" }
                 </GridContainer>
             </div>
         );
