ba23933242d850f838fa84e1449fc4c46164f0c2
tiagocmendes <tiagocmendes@ua.pt>
2020-04-12 03:12:39 +0100

    Finished feature

--- src/views/Continents/Continents.js
+++ src/views/Continents/Continents.js
@@ -9,7 +9,12 @@ import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import avatar from "assets/img/faces/marc.jpg";
+import continent1 from "assets/img/continents-01.png";
+import continent2 from "assets/img/continents-02.png";
+import continent3 from "assets/img/continents-03.png";
+import continent4 from "assets/img/continents-04.png";
+import continent5 from "assets/img/continents-05.png";
+import continent6 from "assets/img/continents-06.png";
 
 import Countries from "views/Continents/Countries.js";
 
@@ -22,41 +27,77 @@ class Continents extends React.Component {
         this.state = {
             continents: null,
             selectedContinent: null,
-            redirect: false 
+            redirect: false
         }
+        this.images = [continent1, continent2, continent3, continent4, continent5, continent6];
+        this.countriesPage = this.countriesPage.bind(this);
     }
 
     componentDidMount() {
 
-        fetch(baseUri.)
+        fetch(baseUri.endpoints.continents, {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        })
+            .then(response => {
+                if (!response.ok) throw new Error(response.status);
+                else return response.json();
 
+            })
+            .then(data => {
+                var continents = [];
+                data.forEach(element => {
+                    continents.push(element.name);
+                });
+                this.setState({
+                    continents: continents.sort(),
+                    selectedContinent: this.state.selectedContinent,
+                    redirect: this.state.redirect
+                })
+            })
+            .catch(error => {
+                console.log("Fetch error: " + error);
+            })
 
     }
 
+    countriesPage(continent) {
+        this.setState({
+            selectedContinent: continent,
+            redirect: true
+        })
+    }
+
     render() {
-        if(this.state.redirect) return <Countries continent={this.state.selectedContinent} />
+        if (this.state.redirect) return <Countries continent={this.state.selectedContinent} />
         return (
             <div>
                 <GridContainer>
-                    <GridItem xs={12} sm={12} md={12}>
+                    <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-50px", marginBottom: "20px" }}>
                         <h3><i className="fas fa-globe-europe"></i> Continents</h3>
                     </GridItem>
-                    <GridItem xs={12} sm={12} md={4} style={{marginTop: "30px"}}>
-                        <Card profile>
-                            <CardAvatar profile>
-                                <a href="#pablo" onClick={e => e.preventDefault()}>
-                                    <img src={avatar} alt="..." />
-                                </a>
-                            </CardAvatar>
-                            <CardBody profile>
-                                <h3>Europe</h3>
-                                <Button color="success" round>
-                                    Countries
-                                </Button>
-                            </CardBody>
-                        </Card>
-                    </GridItem>
-
+                    {this.state.continents !== null ?
+                        this.state.continents.map((continent, key) => {
+                            return <GridItem xs={12} sm={12} md={4} style={{ marginTop: "10px" }}>
+                                <Card profile>
+                                    <CardAvatar profile>
+                                        <a href="/">
+                                            <img src={this.images[key]} alt="..." />
+                                        </a>
+                                    </CardAvatar>
+                                    <CardBody profile>
+                                        <h3>{continent}</h3>
+                                        <Button color="success" round onClick={() => this.countriesPage(continent)}>
+                                            Countries
+                                        </Button>
+                                    </CardBody>
+                                </Card>
+                            </GridItem>
+                        })
+                        : ""}
                 </GridContainer>
             </div>
         );
--- src/views/Continents/Countries.js
+++ src/views/Continents/Countries.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
+import ArrowBack from '@material-ui/icons/ArrowBackIos';
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import IconButton from '@material-ui/core/IconButton';
+import Table from "components/Table/Table.js";
+
+import Continents from "views/Continents/Continents.js";
+
 class Countries extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            return: false
+        }
+        this.toggleReturn = this.toggleReturn.bind(this);
+    }
+
+    toggleReturn() {
+        this.setState({
+            return: true
+        })
+    }
+
+
     render() {
+        if(this.state.return) return <Continents />
         return (
-            <div><h1>Countries</h1></div>
+            <div>
+                 <GridContainer>
+                    <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-50px", marginBottom: "20px" }}>
+                    <h3><IconButton aria-label="back">
+                            <ArrowBack onClick={() => this.toggleReturn()} style={{ color: "#4caf50" }} fontSize="medium" />
+                        </IconButton>
+                        Countries</h3>
+                    </GridItem>
+                </GridContainer>
+            </div>
         )
     }
 
