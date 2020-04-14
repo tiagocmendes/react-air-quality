59496deed6ae501488cd3d5a6545f546a0ee0cee
tiagocmendes <tiagocmendes@ua.pt>
2020-04-14 04:16:35 +0100

    Removed other caches

--- src/views/CacheAnalytics/CacheAnalytics.js
+++ src/views/CacheAnalytics/CacheAnalytics.js
@@ -37,7 +37,7 @@ class CacheAnalytics extends React.Component {
             <div>
                 <GridContainer>
                     <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-50px", marginBottom: "20px" }}>
-                        <h3><i class="fas fa-hdd"></i> <strong>All caches</strong></h3>
+                        <h3><i class="fas fa-hdd"></i> <strong>Cache analytics</strong></h3>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={4} style={{ marginTop: "10px" }}>
                         <Card profile>
@@ -49,36 +49,6 @@ class CacheAnalytics extends React.Component {
                             <CardBody profile>
                                 <h3>Air Quality Cache</h3>
                                 <Button onClick={() => this.cacheDetails(1)} color="success" round>
-                                    Details
-                                        </Button>
-                            </CardBody>
-                        </Card>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={4} style={{ marginTop: "10px" }}>
-                        <Card profile>
-                            <CardAvatar profile>
-                                <a href="/">
-                                    <img src={cache02} alt="..." />
-                                </a>
-                            </CardAvatar>
-                            <CardBody profile>
-                                <h3>Continents Cache</h3>
-                                <Button color="success" onClick={() => this.cacheDetails(2)} round>
-                                    Details
-                                        </Button>
-                            </CardBody>
-                        </Card>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={4} style={{ marginTop: "10px" }}>
-                        <Card profile>
-                            <CardAvatar profile>
-                                <a href="/">
-                                    <img src={cache03} alt="..." />
-                                </a>
-                            </CardAvatar>
-                            <CardBody profile>
-                                <h3>Countries Cache</h3>
-                                <Button color="success" onClick={() => this.cacheDetails(3)} round>
                                     Details
                                         </Button>
                             </CardBody>
